d0e8200323dbdaecae7f772082083e4684dc7c04
singhalharsh1611 <singhalharsh1611@gmail.com>
2026-04-14 21:11:02 +0530

    added redis url in analyse profile job

--- backend/src/jobs/analyzeProfileJob.js
+++ backend/src/jobs/analyzeProfileJob.js
@@ -1,6 +1,18 @@
 import { Queue } from "bullmq";
 import IORedis from "ioredis";
+import dotenv from "dotenv";
 
-const connection = new IORedis();
+dotenv.config();
+
+const REDIS_URL = process.env.REDIS_URL;
+
+if (!REDIS_URL) {
+	throw new Error("❌ REDIS_URL missing in analyzeProfileJob");
+}
+
+const connection = new IORedis(REDIS_URL, {
+	maxRetriesPerRequest: null,
+	db: 0,
+});
 
 export const analyzeProfileQueue = new Queue("analyzeProfile", { connection });
